8c24a39a2744d8c4ca1de58d4a169e072ce9d824
Erica Sun <ericasun@seas.upenn.edu>
2020-06-03 22:53:02 +0000

    tested sending move data to server

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net = require('net');
 
-// const connectMessage = () => console.log('Successfully connected to game server');
+const connectMessage = () => console.log('Successfully connected to game server');
+// const sendMessage = (conn, message) => conn.write(message);
 
 /**
  * Establishes connection with the game server
@@ -12,10 +13,14 @@ const connect = function(data) {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-  conn.on('connect', () => console.log('Successfully connected to game server'));
-  conn.on('data', data => console.log(data));
-  conn.on('connect', () => conn.write('Name: ES'));
+  conn.on('connect', connectMessage); // on function doesnt want parameters for the function
+  conn.on('connect', () => {
+    conn.write('Name: ES');
+    // setInterval(() => conn.write('Move: up'), 50); (callback does not take in parameters)
+    // setTimeout(() => conn.write('Move: left'), 100); (callback does not take in parameters)
+  });
 
+  conn.on('data', data => console.log(data));
   return conn;
 }
 
