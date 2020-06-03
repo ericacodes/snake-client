ad95f110d2012f55d9258a5ec5ad75382b5e9517
Erica Sun <ericasun@seas.upenn.edu>
2020-06-03 23:29:48 +0000

    set up stdin

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  console.log(key);
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+setupInput();
