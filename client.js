5026f2d33ca3918e0aa9cdd6ae42fbfc2104af32
Erica Sun <ericasun@seas.upenn.edu>
2020-06-03 18:33:01 +0000

    moved connection logic into client.js

---
+++ client.js
@@ -0,0 +1,15 @@
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = connect; 
