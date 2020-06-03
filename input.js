a0a8f6d20b73317b8ddcfffb9307525943dc4ca0
Erica Sun <ericasun@seas.upenn.edu>
2020-06-03 23:51:04 +0000

    implemented message

--- input.js
+++ input.js
@@ -26,6 +26,9 @@ const handleUserInput = function(key) {
   if (key === 'd') {
     connection.write('Move: right');
   }
+  if (key === 'q') {
+    connection.write('Say: fun');
+  }
 };
 
 module.exports = { setupInput };
